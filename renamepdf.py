22431b71d6e22bce4f6fb607616295d8ded7b5be
Bryant Novas <bryant.novas@gmail.com>
2017-11-07 20:26:34 -0500

    fixes bugs in search regex, deletes unecessary line breaks on file rename and command line path validation

--- renamepdf.py
+++ renamepdf.py
@@ -1,9 +1,9 @@
-#! python3
+#!python3
 # A simple script to rename pdf report cards based on student info
 
 import os, PyPDF2, re, sys
 
-regex_text = r'\w+,.*?\(\d+\)'
+regex_text = r'(\w+ )?\w+,.*?\(\d+\)'
 search_regex = re.compile(regex_text, re.DOTALL)
 # searches for student info using regexes
 
@@ -11,9 +11,9 @@ count = 0
 # counts the number of files changed
 
 if(len(sys.argv) < 2):
-  print(r'''usage: python3 renamepdf.py  '[path to folder] ' - renames all files in folder''')
+  print('''Usage: python3 renamepdf.py  '[path to folder] ' - renames all files in folder''')
   exit()
-elif(os.path.exists(sys.argv[1]) == False):
+elif(os.path.exists(str(sys.argv[1])) == False):
   print('Invalid path please get the right path and try again')
   exit()
 # Verifies that user typed a path and if the path exists
@@ -28,7 +28,8 @@ for root, subfolders, filenames in os.walk(sys.argv[1]):
     search_name = search_regex.search(get_text)
     if(search_name != None ):
       get_name = search_name.group()
-      new_name = root + '/' + get_name + '.pdf'
+      remove_lines = re.sub(r'\s+', ' ', get_name)
+      new_name = root + '/' + remove_lines + '.pdf'
       os.rename(file_path, new_name)
       get_pdf.close()
       count += 1
